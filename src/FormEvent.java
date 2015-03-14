011fd9253e43a48a4264ff987c119e53722bac80
H <heshamscorpe55@gmail.com>
2015-03-14 20:18:58 +0200

    Makes the variables within the event accessable

--- src/FormEvent.java
+++ src/FormEvent.java
@@ -18,4 +18,20 @@ public class FormEvent extends EventObject {
         this.username = username;
         this.email = email;
     }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
