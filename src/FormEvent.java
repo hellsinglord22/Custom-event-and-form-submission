6e1bff2b711eb6514faddb4df5c62577e0a314ff
H <heshamscorpe55@gmail.com>
2015-03-14 20:13:42 +0200

    Creats an event to carry out info. from t. from the Form to MainFrame

---
+++ src/FormEvent.java
@@ -0,0 +1,12 @@
+import java.util.EventObject;
+
+/**
+ * @Author: Scorpion
+ * @Date: 3/14/2015.
+ */
+public class FormEvent extends EventObject {
+
+    public FormEvent(Object source) {
+        super(source);
+    }
+}
