b6113787a86b7e923cb8adbf403fac7aa3b159f2
H <heshamscorpe55@gmail.com>
2015-03-15 19:35:10 +0200

    adds comments for clearfying

--- src/FormEvent.java
+++ src/FormEvent.java
@@ -9,28 +9,26 @@ public class FormEvent extends EventObject {
     /*Attributes*/
     private String username , email;
 
+    /*Constructor*/
     public FormEvent(Object source) {
         super(source);
     }
-
     public FormEvent(Object source, String username, String email) {
         super(source);
         this.username = username;
         this.email = email;
     }
 
+    /*Getter and setters*/
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
-
     public String getUsername() {
         return username;
     }
-
     public void setUsername(String username) {
         this.username = username;
     }
